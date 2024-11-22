ce2c7c585371b21c40c26bf772ac03bcd3f41c7f
JessiP23 <jessi316866@gmail.com>
2024-11-21 22:30:38 -0500

    hook

--- radio-button.js
+++ radio-button.js
@@ -1,1 +1,18 @@
 // create my own custom elements in html
+
+// extending the native html element
+class RadioButton extends HTMLElement {
+    // this is just for like making sure that properly setup the current class
+    constructor() {
+        super();
+    }
+
+    // every custom element has its own lifecycle methods that hook to run my own code
+    connectedCallback() {
+        this.setAttribute('role', 'radio');
+        this.setAttribute('tabindex', -1);
+        this.setAttribute('aria-checked', 'false'); 
+    }
+}
+
+window.customElements.define('radio-button', RadioButton);
